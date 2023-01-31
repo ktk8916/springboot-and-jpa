fdbfc4a317e6cbd7cfe54864170981f049c28d28
ktk8916 <ktk8916@naver.com>
2023-02-01 00:07:48 +0900

    웹 계층

---
+++ src/test/java/jpabook/jpashop/service/ItemUpdateTest.java
@@ -0,0 +1,16 @@
+package jpabook.jpashop.service;
+
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.persistence.EntityManager;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ItemUpdateTest {
+
+    @Autowired
+    EntityManager em;
+}
